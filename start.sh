f4dddb80d7583ac1630779bfe7d1473f5ac11478
GRZLY53 <125017994+GRZLY53@users.noreply.github.com>
2025-05-18 19:52:35 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,23 @@
+echo "=== Installing packages ==="
 apt-get update
 apt-get install -y git ffmpeg sox
 
+echo "=== Cloning Coqui ==="
 cd /workspace
 git clone https://github.com/coqui-ai/TTS.git
 cd TTS
+
+echo "=== Installing Python dependencies ==="
 pip install -e .
 pip install matplotlib pandas librosa unidecode
 
+echo "=== Downloading model ==="
 python3 -m TTS.api.cli --model_name tts_models/de/thorsten/tacotron2-DDC --download
 
+echo "=== Generating test.wav ==="
 python3 -m TTS.api.cli \
   --text "Hallo, ich teste Coqui TTS auf RunPod." \
   --model_name tts_models/de/thorsten/tacotron2-DDC \
   --out_path /workspace/test.wav
+
+echo "=== DONE ==="
