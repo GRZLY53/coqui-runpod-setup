6e6ac16725515a48701ad62aa264236432368f32
GRZLY53 <125017994+GRZLY53@users.noreply.github.com>
2025-05-18 19:39:34 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
-apt-get update && apt-get install -y git ffmpeg sox && \
-cd /workspace && \
-git clone https://github.com/coqui-ai/TTS.git && \
-cd TTS && \
-pip install -e . && \
-pip install matplotlib pandas librosa unidecode && \
-tts --model_name tts_models/de/thorsten/tacotron2-DDC --download && \
+apt-get update
+apt-get install -y git ffmpeg sox
+
+cd /workspace
+git clone https://github.com/coqui-ai/TTS.git
+cd TTS
+pip install -e .
+pip install matplotlib pandas librosa unidecode
+
+tts --model_name tts_models/de/thorsten/tacotron2-DDC --download
+
 tts --text "Hallo, ich teste Coqui TTS auf RunPod." \
     --model_name tts_models/de/thorsten/tacotron2-DDC \
     --out_path /workspace/test.wav
